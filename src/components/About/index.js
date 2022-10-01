e9f871ed2c72799be3c90f4738703a9c6fab006a
Mert Arinci <mertarinci96@gmail.com>
2022-10-02 00:37:58 +0300

    about and little touch on logo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,25 @@
 import "./index.scss"
 
 import React from 'react'
+import AnimatedLetters from "../AnimatedLetters"
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="container about-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+                        idx={15} />
+                </h1>
+                <p>I am Mert from Turkey. I finished my bachelor degree in Economics and Administrative Sciences. Coding and developing were always my passion since i was in high school. First, i started my coding journey with Python and than i discovered the world of "web and mobile app development".</p>
+                <p>
+                    I'm quietly confident, highly curious and always working hard to improve myself in this field. I really love solving problems and struggling with bugs for hours. As said : "Computer says no!"
+                </p>
+                <p>
+                    In the end, if i need to define myself in one sentence that would be a curious, eager to learn, team player, a sports fanatic, family person and tech-obsessed!
+                </p>
+            </div>
+        </div>
     )
 }
 
